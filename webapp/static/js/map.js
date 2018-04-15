b24a4cda00ccc43063d0b1ded7a81800cc983aad
Paula Mendez <paulipotter@gmail.com>
2018-04-15 15:37:26 -0500

    working on map

--- webapp/static/js/map.js
+++ webapp/static/js/map.js
@@ -2,11 +2,12 @@ $('document').ready(function(){
 
     console.log('inside map js')
 
-    var m_width = $('#map').width(),
-    width = 938,
-    height = 500,
-    country,
-    state;
+    //create variables
+    var m_width = $("#map").width(),
+        width = 938,
+        height = 500,
+        country,
+        state;
 
     var projection = d3.geo.mercator()
         .scale(150)
@@ -15,83 +16,85 @@ $('document').ready(function(){
     var path = d3.geo.path()
         .projection(projection);
 
-    var svg = d3.select('#map').append('svg')
-        .attr('preserveAspectRatio', 'xMidYMid')
-        .attr('viewBox', '0 0 ' + width + " " + height)
-        .attr('width', m_width)
-        .attr('height', m_width * height / width);
+    var svg = d3.select("#map").append("svg")
+        .attr("preserveAspectRatio", "xMidYMid")
+        .attr("viewBox", "0 0 " + width + " " + height)
+        .attr("width", m_width)
+        .attr("height", m_width * height / width);
 
-    svg.append('rect')
-        .attr('class', 'background')
-        .attr('width', width)
-        .attr('height', height)
-    //.on('click', country_clicked);
+    svg.append("rect")
+        .attr("class", "background")
+        .attr("width", width)
+        .attr("height", height)
 
-    var g = svg.append('g');
+    var g = svg.append("g");
 
-    load_country(g,path);
+    d3.json("/static/maps/countries.topo.json", function(error, topology) {
+      g.append("g")
+        .attr("id", "countries")
+        .selectAll("path")
+        .data(topology.features)
+        .enter()
+        .append("path")
+        .attr("id", function(d) { return d.id; })
+        .attr("d", path)
+    });
 
-    g.selectAll(["#states", "#cities"]).remove();
-    state = null;
+    d3.json("/static/maps/states.topo.json", function(error, topology) {
+      g.append("g")
+        .attr("id", "states")
+        .selectAll("path")
+        .data(topology.features)
+        .enter()
+        .append("path")
+        .attr("id", function(d) { return d.id; })
+        .attr("class", "active")
+        .attr("d", path)
+      });
 
-    var xyz = get_xyz(d);
-    country = d;
+     //zoom(xyz,g,path);
 
-      //load states
-    load_city(g,path);
+     country_code = "PRY"
+     //state_name = state.properties.name;
+
+     d3.json("/static/maps/cities.topo.json", function(error, topology) {
+       g.append("g")
+         .attr("id", "cities")
+         .selectAll("path")
+         .data(topology.features)
+         .enter()
+         .append("path")
+         .attr("id", function(d) { return d.properties.name; })
+         .attr("class", "city")
+         .attr("d", path.pointRadius(20 / xyz[2]));
+
+         });
+
+
 
     $(window).resize(function() {
-        var w = $("#map").width();
-        svg.attr("width", w);
-        svg.attr("height", w * height / width);
+      var w = $("#map").width();
+      svg.attr("width", w);
+      svg.attr("height", w * height / width);
     });
 
 });
-
-function load_country(g,path) {
-    //setup country
-    d3.json('/static/maps/countries.topo.json', function(error, topology) {
-    g.append('g')
-      .attr('id', 'countries')
-      .selectAll('path')
-      .data(topology.features)
-      .enter()
-      .append('path')
-      .attr('id', function(d) { return d.id; })
-      .attr('d', path)
-    });
-}//end load country
-
-function load_state(g, path) {
-
-      d3.json('/static/maps/states.topo.json', function(error, topology) {
-        g.append('g')
-          .attr('id', 'states')
-          .selectAll('path')
-          .data(topology.features)
-          .enter()
-          .append('path')
-          .attr('id', function(d) { return d.id; })
-          .attr('class', 'active')
-          .attr('d', path)
-
-        zoom(xyz);
-        g.selectAll('#' + d.id).style('display', 'none');
-      });
-
+function zoom(xyz,g,path) {
+  g.transition()
+    .duration(750)
+    .attr("transform", "translate(" + projection.translate() + ")scale(" + xyz[2] + ")translate(-" + xyz[0] + ",-" + xyz[1] + ")")
+    .selectAll(["#countries", "#states", "#cities"])
+    .style("stroke-width", 1.0 / xyz[2] + "px")
+    .selectAll(".city")
+    .attr("d", path.pointRadius(20.0 / xyz[2]));
 }
 
-function load_city() {
-  d3.json("/static/maps/cities.topo.json", function(error, topology) {
-      g.append("g")
-        .attr("id", "cities")
-        .selectAll("path")
-        .data(topology.features.filter(function(d) { return state_name == d.properties.state; }))
-        .enter()
-        .append("path")
-        .attr("id", function(d) { return d.properties.name; })
-        .attr("class", "city")
-        .attr("d", path.pointRaditopology(20 / xyz[2]));
-
-      zoom(xyz);
+function get_xyz(d,path) {
+  var bounds = path.bounds(d);
+  var w_scale = (bounds[1][0] - bounds[0][0]) / width;
+  var h_scale = (bounds[1][1] - bounds[0][1]) / height;
+  var z = .96 / Math.max(w_scale, h_scale);
+  var x = (bounds[1][0] + bounds[0][0]) / 2;
+  var y = (bounds[1][1] + bounds[0][1]) / 2 + (height / z / 6);
+  return [x, y, z];
 }
