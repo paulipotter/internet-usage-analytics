23bca583680dad1ecbd29ada97dea98b449c1855
Paula Mendez <paulipotter@ksu.edu>
2018-05-10 00:17:18 -0500

    scroll reveal

--- webapp/static/js/results.js
+++ webapp/static/js/results.js
@@ -1,21 +1,9 @@
 $(document).ready(function(){
-    window.sr = ScrollReveal();
+    window.sr = ScrollReveal({ reset: true });
     console.log('results')
 
-
     //title
-    window.setTimeout( function() {
-        var title = new Typed('#title', options)
-        $('.typed-cursor').hide();
-    },1000);
-
-    sr.reveal('#description', { container: '#description' });
-
-    animateValue('entries', 0, 900, 1500);
-
-    var op = {strings : ['  people completed the survey'], typeSpeed: 40};
-        var entries_txt = new Typed('#entries-txt',op);
-
+    sr.reveal('#title', { duration: 200 });
     var options = {strings: ['Senior Project: Internet Usage in Paraguay'],
         typeSpeed:40,
         callback: function () {
@@ -24,17 +12,46 @@ $(document).ready(function(){
     }
                 $('.typed-cursor').hide();
 
+        var title = new Typed('#title', options)
+        $('.typed-cursor').hide();
 
 
+    sr.reveal('#description', {
+      origin: 'right',
+      duration: 2000
+    });
+
+    sr.reveal('#ent1', {
+      rotate: { x: 100, y: 100, z: 0 },
+      duration: 1000
+    });
+    animateValue('entries', 0, 900, 1500);
+    var op = {strings : ['  people completed the survey'], typeSpeed: 40};
+        var entries_txt = new Typed('#entries-txt',op);
+        $('.typed-cursor').hide();
+    sr.reveal('.#ent2', {
+      viewFactor: 0.5
+    });
     var tv_txt = {strings: ['watch TV as means to get informed on news'],typeSpeed:40}
     var tv = new Typed('#tv-txt', tv_txt)
-
+    sr.reveal('#ent3', {
+      duration: 200
+    });
     var mi_txt = {strings: [counts["millenials"]["string"]],typeSpeed:40}
     var mi = new Typed('#mi-txt', mi_txt)
-
+    sr.reveal('#ent4', {
+      duration: 200
+    });
     var bb_txt = {strings: [counts["baby_boomers"]["string"]],typeSpeed:40}
     var bb = new Typed('#bb-txt', bb_txt)
     $('.typed-cursor').hide();
+
+    // GENERAL SETTING
+
+    // Custom Settings
+
+
+
 });
 // https://stackoverflow.com/questions/16994662/count-animation-from-number-a-to-b
 function animateValue(id, start, end, duration) {
